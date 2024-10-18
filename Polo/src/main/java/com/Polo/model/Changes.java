c32a18d3997a83b7d4c71a633fc16f0827fa38f2
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2024-10-17 22:18:13 -0300

    Mappeo de entidades seleccionadas

--- Polo/src/main/java/com/Polo/model/Changes.java
+++ Polo/src/main/java/com/Polo/model/Changes.java
@@ -1,5 +1,43 @@
 package com.Polo.model;
 
-public class Changes {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Changes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_changes", nullable = false)
+    private Integer id;
+
+    @Basic
+    @Column(name = "change_description", nullable = false, columnDefinition = "TEXT")
+    private String changesDescription;
+
+    @CreationTimestamp
+    @Column(name = "change_date", nullable = false, updatable = false)
+    private LocalDateTime changesDate;
+
+    @Column(name = "change_thing", nullable = false, columnDefinition = "TEXT")
+    private String changesThing;
+
+    @Column(name = "change_type", nullable = false)
+    private String changesType;
+
+    @ManyToOne
+    @JoinColumn(name = "change_iduser", nullable = false) // Clave foránea
+    private User changeIdUser;
+
+    // Falta relacion ManyToMany con User y con Project
 }
--- Polo/src/main/java/com/Polo/model/Postulation.java
+++ Polo/src/main/java/com/Polo/model/Postulation.java
@@ -1,5 +1,28 @@
 package com.Polo.model;
 
-public class Postulation {
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
+public class Postulation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_postulation", nullable = false)
+    private Integer id;
+
+    @Basic
+
+    @Column(name = "postulation_name", nullable = false)
+    private String postulationName;
+
+    @Column(name = "postulation_description", nullable = false, columnDefinition = "TEXT")
+    private String postulationDescription;
+
+    // Falta relacion ManyToMany con User y con Project
 }
