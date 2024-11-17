40268f9902e2c7ba29fb94813929055e627bbb03
diego arteaga mendoza <daarteagamendoza@gmail.com>
2024-11-17 20:45:55 -0300

    Trabajo en updateEnvironmentVinculation (por terminar)

---
+++ Polo/src/main/java/com/Polo/repository/EnvironmentVinculationRepository.java
@@ -0,0 +1,13 @@
+package com.Polo.repository;
+
+import com.Polo.model.EnvironmentVinculation;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnvironmentVinculationRepository extends JpaRepository<EnvironmentVinculation, Integer> {
+    Optional<EnvironmentVinculation> findByActivityName(String environmentVinculationName);
+}
