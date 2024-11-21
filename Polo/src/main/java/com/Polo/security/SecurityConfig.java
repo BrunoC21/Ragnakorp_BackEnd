c73f13e026db101a113577c3a4bae2cd9fa6fb62
diego arteaga mendoza <daarteagamendoza@gmail.com>
2024-11-20 22:15:04 -0300

    Liberacion de url para buscar noticias

--- Polo/src/main/java/com/Polo/security/SecurityConfig.java
+++ Polo/src/main/java/com/Polo/security/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/user/login", "/user/create", "/user/search", "/user/search/rut/**", "/user/assignRole/**", "/project/create/**", "/news/create/**", "/project/search", "/user/sessionInfo").permitAll() // Permitir acceso sin autenticación
+                .requestMatchers("/user/login", "/user/create", "/user/search", "/user/search/rut/**", "/user/assignRole/**", "/project/create/**", "/news/create/**", "/project/search", "/user/sessionInfo", "/news/search").permitAll() // Permitir acceso sin autenticación
                 .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
